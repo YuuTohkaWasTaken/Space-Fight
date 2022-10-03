958caca0bf419c056293716c4dca55ac70fb8c00
set <set>
2022-10-03 14:23:00 -0400

    Renamed and added battle, lobby, and home scenes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, PointLight } from "@babylonjs/core";
 
 class App {
     constructor() {
@@ -9,33 +9,23 @@ class App {
         var canvas = document.createElement("canvas");
         canvas.style.width = "100%";
         canvas.style.height = "100%";
+        canvas.style.position = "absolute";
         canvas.id = "gameCanvas";
         document.body.appendChild(canvas);
 
         // initialize babylon scene and engine
         var engine = new Engine(canvas, true);
-        var scene = new Scene(engine);
+        var battlefield = new Scene(engine);
+        var lobby = new Scene(engine);
+        var home = new Scene(engine);
 
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-        camera.attachControl(canvas, true);
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
-
-        // hide/show the Inspector
-        window.addEventListener("keydown", (ev) => {
-            // Shift+Ctrl+Alt+I
-            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
-                if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide();
-                } else {
-                    scene.debugLayer.show();
-                }
-            }
-        });
+        var battlelight: HemisphericLight = new HemisphericLight("light", new Vector3(1, 1, 0), battlefield);
+        var lobbylight: PointLight = new PointLight("light", new Vector3(0, 3, 0), lobby);
+        var homelight: PointLight = new PointLight("light", new Vector3(0, 3, 0), home);
 
         // run the main render loop
         engine.runRenderLoop(() => {
-            scene.render();
+            battlefield.render();
         });
     }
 }
